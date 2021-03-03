8cb3c4fba8ec905879d48e808386fbc5af956bb5
kminny <darkdevil94@naver.com>
2021-03-03 11:31:42 +0900

    Search-bar moved to base & Search options

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import DetailView, ListView
+from django_countries import countries
 from . import models
 
 
@@ -29,7 +30,25 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    room_types = models.RoomType.objects.all()
 
-    return render(request, "rooms/search.html", {"city": city})
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices},
+    )
