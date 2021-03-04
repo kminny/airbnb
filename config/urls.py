cdd6257c8bcb0f909b575cde219d8871043e4a71
kminny <darkdevil94@naver.com>
2021-03-04 12:35:39 +0900

    Validating User

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
-    path("rooms/", include("rooms.urls", namespace="rooms")),
     path("admin/", admin.site.urls),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="login")),
 ]
 
 if settings.DEBUG:
