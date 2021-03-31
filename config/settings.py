77440d25044e07c572806abd7508a342164451ed
kminny <darkdevil94@naver.com>
2021-03-31 19:43:58 +0900

    Debug true

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "em*#%42j7r#t=(c%g9*y9=6)%9pn2)wh--32%0g8lgfef(4c)d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+# DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "http://airbnb-clone.eba-mrzzmnjm.ap-northeast-2.elasticbeanstalk.com/",
@@ -94,7 +95,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
